b3d82227f13c742da13987a1394cf6064cb2e593
LutfiArdiansyah <37618863+LutfiArdiansyah@users.noreply.github.com>
2019-06-11 20:50:32 +0700

    Test HTTPS

--- php_curl_wsdl.php
+++ php_curl_wsdl.php
@@ -1,24 +1,19 @@
 <?php
-
+$url = "https://cdaweb.sci.gsfc.nasa.gov:443/WS/jaxrpc"; // URL WSDL SOAP
+// XML Parameter SOAP
+$xml = "<env:Envelope env:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/' xmlns:env='http://schemas.xmlsoap.org/soap/envelope/' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:enc='http://schemas.xmlsoap.org/soap/encoding/' xmlns:ns0='http://cdaweb.gsfc.nasa.gov/WS/types/CDASWS' xmlns:ns1='http://java.sun.com/jax-rpc-ri/internal'> <env:Body> <ans1:getAllInstrumentDescriptionsResponse xmlns:ans1='http://cdaweb.gsfc.nasa.gov/WS/CDASWS'> <result xsi:type='ns0:ArrayOfInstrumentDescription' enc:arrayType='ns0:InstrumentDescription[0]' xsi:nil='1'/> </ans1:getAllInstrumentDescriptionsResponse> </env:Body> </env:Envelope>";
+// END XML Parameter SOAP
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-	CURLOPT_URL => "http://www.dneonline.com/calculator.asmx",
+	CURLOPT_URL => $url,
 	CURLOPT_RETURNTRANSFER => true,
 	CURLOPT_ENCODING => "",
 	CURLOPT_MAXREDIRS => 10,
 	CURLOPT_TIMEOUT => 30,
 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 	CURLOPT_CUSTOMREQUEST => "POST",
-	CURLOPT_POSTFIELDS => "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tem=\"http://tempuri.org/\">
-	<soapenv:Header/>
-	<soapenv:Body>
-	<tem:Multiply>
-	<tem:intA>4</tem:intA>
-	<tem:intB>2</tem:intB>
-	</tem:Multiply>
-	</soapenv:Body>
-	</soapenv:Envelope>",
+	CURLOPT_POSTFIELDS => $xml,
 	CURLOPT_HTTPHEADER => array(
 		"Content-Type: text/xml",
 		"cache-control: no-cache"
@@ -29,13 +24,12 @@ $response = curl_exec($curl);
 $err = curl_error($curl);
 
 curl_close($curl);
-
+header('Content-Type: application/json');
 if ($err) {
-	echo "cURL Error #:" . $err;
+	$array = json_encode(array('Error' => $err),JSON_PRETTY_PRINT);
 } else {
 	$response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $response);
 	$xml = new SimpleXMLElement($response);
-	$array = json_encode((array)$xml); 
-	print_r($array);die();
-	$this->output->set_content_type('application/json')->set_output(json_encode($array));
+	$array = json_encode((array)$xml,JSON_PRETTY_PRINT); 
 }
+echo $array;
